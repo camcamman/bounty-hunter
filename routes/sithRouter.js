5df6fa28f4a7cdef3b21bfb6da2ab9547706808a
Cameron Ford <c.r.ford@outlook.com>
2023-01-09 15:16:14 -0700

    bounty hunter pt1 is done

--- routes/sithRouter.js
+++ routes/sithRouter.js
@@ -1,26 +1,32 @@
 const express = require(`express`)
 const sithRouter = express.Router()
 const { v4: uuidv4 } = require('uuid');
+const jediRouter = require('./jediRouter');
+
+const app = express()
+const port = 3001
 
 const sith = [
-    {fName:"s0", lName:"s0", _id:uuidv4},
-    {fName:"s1", lName:"s1", _id:uuidv4},
-    {fName:"s2", lName:"s2", _id:uuidv4},
-    {fName:"s3", lName:"s3", _id:uuidv4},
-    {fName:"s4", lName:"s4", _id:uuidv4},
-    {fName:"s5", lName:"s5", _id:uuidv4}
+    {fName:"s0", lName:"s0", _id:uuidv4()},
+    {fName:"s1", lName:"s1", _id:uuidv4()},
+    {fName:"s2", lName:"s2", _id:uuidv4()},
+    {fName:"s3", lName:"s3", _id:uuidv4()},
+    {fName:"s4", lName:"s4", _id:uuidv4()},
+    {fName:"s5", lName:"s5", _id:uuidv4()}
 ]
 
 // sithRouter.route("/")
-// sithRouter.get("/", (req, res) => {
+//     .get("/", (req, res) => {
 //     res.send(sith)
 //  })
-//  .post((req, res) => {
+//     .post((req, res) => {
 //     const newSith = req.body
 //     newSith._id = uuidv4
 //     sith.push(newSith)
 //     res.send(`New sith was added`)
 //  })
+
+
 
 sithRouter.get("/", (req, res) => {
     res.send(sith)
@@ -31,5 +37,8 @@ sithRouter.post("/", (req, res) => {
     newSith._id = uuidv4
     sith.push(newSith)
     res.send(`New sith was added `)
-    // res.send(newJedi)
 })
+
+app.use(express.json())
+
+module.exports = sithRouter
